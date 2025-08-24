7108d57d3318017ed59a166394a3308df9431d8d
Darichel <darichel.perez@gmail.com>
2025-08-24 13:31:14 -0300

    Usando zoom enla app

--- src/app/pages/fullscreen-map-page/fullscreen-map-page.component.ts
+++ src/app/pages/fullscreen-map-page/fullscreen-map-page.component.ts
@@ -1,37 +1,81 @@
-import { AfterViewInit, Component, ElementRef, viewChild } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  effect,
+  ElementRef,
+  signal,
+  viewChild,
+} from '@angular/core';
 
 import mapboxgl from 'mapbox-gl';
 import { environment } from '../../../environments/environment';
+import { DecimalPipe } from '@angular/common';
 
 mapboxgl.accessToken = environment.MAPBOX_KEY;
 
 @Component({
   selector: 'app-fullscreen-map-page',
-  imports: [],
+  imports: [DecimalPipe],
   templateUrl: './fullscreen-map-page.component.html',
   styles: `
     .mapContainer {
       width: 100vw;
       height: calc(100vh - 64px);
     }
+
+    #control {
+      background-color: white;
+      padding: 10px;
+      border-radius: 5px;
+      position: fixed;
+      bottom: 25px;
+      right: 20px;
+      z-index: 999;
+      box-shadow: 0 0 10px 0 rgba(0, 0, 0, 0.1);
+      border: 1px solid #e2e8f0;
+      width: 250px
+    }
   `,
 })
 export class FullscreenMapPageComponent implements AfterViewInit {
   divElement = viewChild<ElementRef>('map');
 
+  map = signal<mapboxgl.Map | null>(null);
+
+  zoom = signal(14);
+
+  zoomEffect = effect(() => {
+    const map = this.map();
+    const zoom = this.zoom();
+
+    if (!map) return;
+
+    map.setZoom(zoom);
+  });
+
   async ngAfterViewInit() {
+    if (!this.divElement()) return;
 
-    if(!this.divElement()) return;
+    await new Promise((resolve) => setTimeout(resolve, 100));
 
-    await new Promise(resolve => setTimeout(resolve, 100));
-
-    const element = this.divElement()! .nativeElement
+    const element = this.divElement()!.nativeElement;
 
     const map = new mapboxgl.Map({
       container: element, // container ID
       style: 'mapbox://styles/mapbox/streets-v12', // style URL
       center: [-74.5, 40], // starting position [lng, lat]
-      zoom: 9, // starting zoom
+      zoom: this.zoom(), // starting zoom
     });
+
+    this.mapListeners(map);
+  }
+
+  mapListeners(map: mapboxgl.Map) {
+    map.on('zoomend', (event) => {
+      const newZoom = map.getZoom();
+      this.zoom.set(newZoom);
+    });
+
+    this.map.set(map);
   }
 }
